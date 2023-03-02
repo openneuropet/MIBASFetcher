76f71d481802425b39f3c90e3ba06f294bb2df3f
Anthony Galassi <28850131+bendhouseart@users.noreply.github.com>
2023-03-02 16:12:30 -0500

    updated fetching from atlas.json files

---
+++ fetch_atlases.py
@@ -0,0 +1,58 @@
+import urllib.request
+import json
+import argparse
+from typing import Union
+from pathlib import Path
+
+
+def load_manifest_urls(file_path: Union[Path, str], dataset_name: str='', version: str='') -> list:
+    url_list = []
+    if Path(file_path).suffix == '.json':
+        if not dataset_name and not version:
+            raise KeyError(f"dataset and version arguments must included to extract url list from json file.\nYou provided dataset: {dataset}, version: {version}.")
+        with open(file_path, 'r') as jsonfile:
+            json_contents = json.load(jsonfile)
+            dataset = json_contents.get(dataset_name, {})
+            version = dataset['version'].get(version, {})
+        return version['urls']
+    else:
+        with open(file_path, 'r') as infile:
+            lines = infile.readlines()
+            lines = [l.strip() for l in lines]
+        for line in lines:
+            filename, url = line.split(' ')
+            url_list['urls'].append({filename: url})
+        return url_list
+
+def download_files(manifest_urls: list, destination: Union[Path, str]=Path('downloads/')) -> None:
+    url_list = manifest_urls
+    for entry in url_list:
+        for name, url in entry.items():
+            try:
+                data = urllib.request.urlopen(url=url, timeout=20)
+                file_path = Path(destination) / Path(name)
+                file_path.parent.mkdir(parents=True, exist_ok=True)
+                open(file_path, 'wb').write(data.read())
+                print(f"Collected {name} at {url}")
+            except urllib.error.HTTPError as err:
+                print(f"Unable to download {name} from {url}")
+                print(err)
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser('fetch_atlases')
+    parser.add_argument('manifest')
+    parser.add_argument('destination')
+    parser.add_argument('-n', '--dataset-name', default='')
+    parser.add_argument('-v', '--dataset-version', default='')
+    args = parser.parse_args()
+
+    urls = load_manifest_urls(args.manifest, dataset_name=args.dataset_name, version=args.dataset_version)
+
+    # set up destination path
+    if args.dataset_name not in str(args.destination) and args.dataset_name:
+        args.destination = Path(args.destination) / Path(args.dataset_name)
+    if args.dataset_version not in str(args.destination) and args.dataset_version:
+        args.destination = Path(args.destination) / Path(args.dataset_version)
+    
+    download_files(urls, destination=args.destination)
--- git_atlases.py
+++ git_atlases.py
@@ -6,7 +6,7 @@ import json
 import os
 
 
-def remove_bids_path(url):
+def extract_path_from_url(url):
     if 'github' in url:
         dataset = re.search(r"ds\d+", url)[0]
         bids_path  = re.search(r"(\d+(?:\.\d+)+)\/(.*)", url)[2]
@@ -70,26 +70,25 @@ for atlas in atlases.keys():
                 url = re.search(r"https:\/\/[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&\/\/=]*)", line)
                 if url:
                     # write to dictionary as well
-                    atlases[atlas]['version'][tag]['urls'].append(remove_bids_path(url[0]))
+                    atlases[atlas]['version'][tag]['urls'].append(extract_path_from_url(url[0]))
                     outfile.write(url[0] + '\n')
-                    print(remove_bids_path(url=url[0]))
+                    print(extract_path_from_url(url=url[0]))
 
         # check all version files for symlinks, if local paths resolve to remote url and add to url list
         with open(f"{working_dir}/{atlas}_{tag}.manifest", 'a') as outfile:
             for root, folders, files in os.walk(os.path.join(working_dir, atlas)):
                 for f in files:
                     f_path = Path(os.path.join(root, f))
-                    if f_path.is_symlink():
+                    if f_path.is_symlink() or '.git' in f_path.parts:
                         pass
                     else:
                         split_f_path = str(f_path).split(atlas, 1)[1]
                         remote_local_file = f"{atlases[atlas]['remote']}/blob/{tag}{split_f_path}"
-                        atlases[atlas]['version'][tag]['urls'].append(remove_bids_path(remote_local_file))
+                        atlases[atlas]['version'][tag]['urls'].append(extract_path_from_url(remote_local_file))
                         outfile.write(remote_local_file + '\n')
-                        print(remove_bids_path(url=remote_local_file))
+                        print(extract_path_from_url(url=remote_local_file))
     
 pretty_json = json.dumps(atlases, indent=2)
 
 with open(f"{working_dir}/atlases.json", 'w') as jsonfile:
     json.dump(atlases, jsonfile, indent=4)
-
